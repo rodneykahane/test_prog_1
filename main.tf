abe9e21a0c558936336f0d9be9f9d2e783024394
Rodney <rjkahane@gmail.com>
2020-07-21 13:22:15 -0400

    replaced port numbers with variable

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+variable "busybox_port"{
+  description = "port used to connect to busybox server"
+  type = number
+  default = 8080
+}
+
 resource "aws_instance" "example" {
   ami                     = "ami-0ac80df6eff0e70b5"
   instance_type           = "t2.micro"
@@ -9,8 +16,8 @@ resource "aws_instance" "example" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello world" > index.html
-              nohup busybox httpd -f -p 8080&
+              ls -l > index.html
+              nohup busybox httpd -f -p ${var.busybox_port} &
               EOF
 
   tags = {
@@ -25,8 +32,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.busybox_port
+    to_port     = var.busybox_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }   
