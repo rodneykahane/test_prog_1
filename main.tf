0cd35c9432d60d6c1adbb2ff3a9ec53d82daef99
Rodney <rjkahane@gmail.com>
2020-07-21 11:53:02 -0400

    added busybox webserver

--- main.tf
+++ main.tf
@@ -2,14 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_instance" "example" {
-  ami           = "ami-0ac80df6eff0e70b5"
-  instance_type = "t2.micro"
+  ami                     = "ami-0ac80df6eff0e70b5"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello world" > index.html
+              nohup busybox httpd -f -p 8080&
+              EOF
 
   tags = {
 
     Name = "terraform-example"
   }
 
-}
+} #end "aws_instance" "example"
+
+resource "aws_security_group" "instance" {
+
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }   
+} #end resource "aws_security_group" "instance"
